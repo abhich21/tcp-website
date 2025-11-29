0c6d6b518d9cef5d2e8dff46fb7afafac35bfbf4
Abhishek Choudhary <96258754+abhich21@users.noreply.github.com>
2025-11-29 18:33:37 +0530

    updated

--- api/index.js
+++
@@ -1,8 +0,0 @@
-let app;
-export default async function handler(req, res) {
-  if (!app) {
-    const appModule = await import('../backend/src/app.js');
-    app = appModule.default;
-  }
-  return app(req, res);
-}
