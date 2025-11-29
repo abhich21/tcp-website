1121297584a9d4e587e5ae2c83c906f7277bc5d4
Abhishek Choudhary <96258754+abhich21@users.noreply.github.com>
2025-11-29 18:22:06 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,12 @@ console.log("Starting server...");
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
-
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+// For local dev
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+  });
+}
+// Export for Vercel
+export default app;
